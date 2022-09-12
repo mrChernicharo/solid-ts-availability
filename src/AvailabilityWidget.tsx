5a1abeb045f75d594d86db73cb7f394762f0f7d4
Felipe Chernicharo <string7dev@gmail.com>
2022-09-12 14:19:48 -0400

    TimeSlots

--- src/AvailabilityWidget.tsx
+++ src/AvailabilityWidget.tsx
@@ -1,7 +1,11 @@
-import { Component, createEffect, createMemo, createSignal, For, Show } from "solid-js";
+import { createPerPointerListeners } from "@solid-primitives/pointer";
+import { batch, Component, createEffect, createMemo, createSignal, For, Show } from "solid-js";
+import { createStore } from "solid-js/store";
 import { SCROLL_BAR, THEME } from "./lib/constants";
-import { IWeekday, IPalette } from "./lib/types";
+import { IWeekday, IPalette, IStore } from "./lib/types";
 import { getLocaleHours } from "./lib/utils";
+// @ts-ignore
+import idMaker from "@melodev/id-maker";
 
 interface IProps {
   locale: string;
@@ -24,10 +28,24 @@ export default function AvailabilityWidget(props: IProps) {
   const HOURS = createMemo(() => getLocaleHours(props.minHour, props.maxHour, props.locale));
 
   const [widgetWidth, setWidgetWidth] = createSignal("0px");
+  const [store, setStore] = createStore<IStore>({
+    slotId: null,
+    slotIdx: null,
+    gesture: "idle",
+    Sun: [],
+    Mon: [
+      { id: idMaker(), height: 50, y: 100 },
+      { id: idMaker(), height: 50, y: 300 },
+    ],
+    Tue: [],
+    Wed: [],
+    Thu: [],
+    Fri: [{ id: idMaker(), height: 50, y: 100 }],
+    Sat: [],
+  });
 
   createEffect(() => {
     const observer = new ResizeObserver((e) => {
-      console.log(e);
       const w = () => {
         if (props.widgetHeight + SCROLL_BAR >= props.colHeight + props.headerHeight) {
           return props.colWidth * (props.dayCols.length + 0.5) + 2;
@@ -42,10 +60,60 @@ export default function AvailabilityWidget(props: IProps) {
     observer.observe(document.body);
   });
 
+  createPerPointerListeners({
+    target: () => containerRef,
+    onEnter(e, { onDown, onMove, onUp, onLeave }) {
+      let last: { x: number; y: number } | null;
+      onDown(({ x, y, offsetY, offsetX }) => {
+        last = { x: offsetX, y: offsetY };
+      });
+      onUp(() => {
+        last = null;
+        setStore("gesture", "idle");
+        console.log("dropped", store.slotId);
+      });
+      onLeave(() => {
+        last = null;
+      });
+      onMove(({ x, y, offsetY, offsetX }) => {
+        if (!last) return;
+        console.log({ e, y: last.y });
+
+        if (store.gesture === "drag:middle") {
+          //   setStore("slots", store.slotIdx!, "pos", (p) => ({
+          //     x: p.x + x - last!.x,
+          //     y: p.y + y - last!.y,
+          //   }));
+        }
+
+        if (store.gesture === "drag:top") {
+          //   batch(() => {
+          //     setStore("slots", store.slotIdx!, "height", (h) => h + (last!.y - y));
+          //     setStore("slots", store.slotIdx!, "pos", (p) => ({
+          //       x: p.x,
+          //       y: p.y + y - last!.y,
+          //     }));
+          //   });
+        }
+
+        if (store.gesture === "drag:bottom") {
+          //   batch(() => {
+          //     setStore("slots", store.slotIdx!, "height", (h) => h + (y - last!.y));
+          //     setStore("slots", store.slotIdx!, "pos", (p) => ({
+          //       x: p.x,
+          //       y: p.y,
+          //     }));
+          //   });
+        }
+
+        last = { x: offsetX, y: offsetY };
+      });
+    },
+  });
+
   return (
     <Show when={props.open}>
       <main
-        ref={containerRef}
         class="mx-auto my-0 overflow-auto flex flex-col whitespace-nowrap"
         style={{
           height: `${props.widgetHeight + SCROLL_BAR + 2}px`,
@@ -108,6 +176,7 @@ export default function AvailabilityWidget(props: IProps) {
 
           {/* ********* TIME GRID ********** */}
           <div
+            ref={containerRef}
             class="relative inline-block"
             style={{
               height: `${props.colHeight}px`,
@@ -116,7 +185,7 @@ export default function AvailabilityWidget(props: IProps) {
           >
             {/* ********* DAY COLS ********** */}
             <For each={props.dayCols}>
-              {(col, colIdx) => (
+              {(day, colIdx) => (
                 <div
                   class="absolute inline-block z-[2] border-l-[1px]"
                   style={{
@@ -125,6 +194,100 @@ export default function AvailabilityWidget(props: IProps) {
                     left: `${props.colWidth * colIdx()}px`,
                   }}
                 >
+                  {/* ********* TIME SLOTS ************ */}
+
+                  <For each={store[day]}>
+                    {(slot, idx) => {
+                      let slotRef!: HTMLDivElement;
+                      let middleRef!: HTMLDivElement;
+                      let topRef!: HTMLDivElement;
+                      let bottomRef!: HTMLDivElement;
+
+                      // SLOT LISTENER
+                      createPerPointerListeners({
+                        target: () => slotRef,
+                        onEnter(e, { onDown, onMove, onUp, onLeave }) {
+                          onDown(({ x, y }) => {
+                            batch(() => {
+                              setStore("slotId", slot.id);
+                              setStore("slotIdx", idx());
+                            });
+                          });
+                        },
+                      });
+
+                      // MIDDLE LISTENER
+                      createPerPointerListeners({
+                        target: () => middleRef,
+                        onEnter(e, { onDown, onMove, onUp, onLeave }) {
+                          onDown(({ x, y }) => {
+                            setStore("gesture", "drag:middle");
+                          });
+                        },
+                      });
+
+                      // TOP LISTENER
+                      createPerPointerListeners({
+                        target: () => topRef,
+                        onEnter(e, { onDown, onMove, onUp, onLeave }) {
+                          onDown(({ x, y }) => {
+                            setStore("gesture", "drag:top");
+                          });
+                        },
+                      });
+
+                      // BOTTOM LISTENER
+                      createPerPointerListeners({
+                        target: () => bottomRef,
+                        onEnter(e, { onDown, onMove, onUp, onLeave }) {
+                          onDown(({ x, y }) => {
+                            setStore("gesture", "drag:bottom");
+                          });
+                        },
+                      });
+
+                      const height = createMemo(() => `${slot.height}px`);
+                      const top = createMemo(() => `${slot.y}px`);
+                      return (
+                        <div
+                          id={slot.id}
+                          ref={slotRef}
+                          class="w-full absolute bg-blue-600"
+                          style={{
+                            top: top(),
+                          }}
+                        >
+                          <div
+                            ref={topRef}
+                            class="w-full h-6  bg-slate-500"
+                            style={{
+                              "touch-action": "none",
+                            }}
+                          ></div>
+                          <div
+                            ref={middleRef}
+                            class="w-full h-[100%] flex flex-col justify-center items-center "
+                            style={{
+                              "touch-action": "none",
+                              "user-select": "none",
+                              height: height(),
+                            }}
+                          >
+                            <p>{slot.id}</p>
+                            <p>{store.slotId === slot.id ? store.gesture : "idle"}</p>
+                          </div>
+                          <div
+                            ref={bottomRef}
+                            class="w-full h-6 bg-slate-500"
+                            style={{
+                              "touch-action": "none",
+                            }}
+                          ></div>
+                        </div>
+                      );
+                    }}
+                  </For>
+
                   {/* ********* HOUR LINES ********** */}
                   <For each={HOURS()}>
                     {(hour, hourIdx) => (
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,3 +2,21 @@ export type IWeekday = "Sun" | "Mon" | "Tue" | "Wed" | "Thu" | "Fri" | "Sat";
 export type IGesture = "idle" | "drag:middle" | "drag:top" | "drag:bottom";
 export type IPalette = "light" | "dark";
 export type ISnapOption = 30 | 20 | 15 | 10 | 5;
+export type IStore = {
+  slotId: string | null;
+  slotIdx: number | null;
+  gesture: "idle" | "drag:middle" | "drag:top" | "drag:bottom";
+  Sun: ITimeSlot[];
+  Mon: ITimeSlot[];
+  Tue: ITimeSlot[];
+  Wed: ITimeSlot[];
+  Thu: ITimeSlot[];
+  Fri: ITimeSlot[];
+  Sat: ITimeSlot[];
+};
+
+type ITimeSlot = {
+  id: string;
+  y: number;
+  height: number;
+};
